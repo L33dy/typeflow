cb84c1bd96a65408bdeca0f49b948e44e8b7a590
L33dy <zbynekzidlicky@gmail.com>
2023-01-19 10:38:39 +0100

    Style changes

--- app/renderer/counter.js
+++ app/renderer/counter.js
@@ -1,8 +1,8 @@
 var editor = document.getElementById("mark-it")
-let bottomPanel = document.getElementById("bottom-panel")
+let counter = document.getElementById("counter")
 
 editor.addEventListener('input', () => {
     const words = editor.textContent.split(" ").filter(function(str) { return str !== ""; });
 
-    bottomPanel.innerText = "Words: " + words.length
+    counter.innerText = "Words: " + words.length
 })
