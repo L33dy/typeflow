93a5ba5769bc121e8028372f00ee3e5c7be5b38f
L33dy <zbynekzidlicky@gmail.com>
2023-01-26 21:10:19 +0100

    brackets.js changes

--- app/renderer/brackets.js
+++ app/renderer/brackets.js
@@ -1,17 +1,39 @@
 var editor = document.getElementById("editor")
+var hasBrackets = false
 
 editor.addEventListener('keydown', (e) => {
     var node = Editor.getCurrentNode()
+    var currentBracket = ''
 
     if(e.key === "(") {
         node.innerText += ")"
+        currentBracket = "()"
+
+        hasBrackets = true
     }
 
     if(e.key === "{") {
         node.innerText += "}"
+        currentBracket = "{}"
+
+        hasBrackets = true
     }
 
     if(e.key === "[") {
         node.innerText += "]"
+        currentBracket = "[]"
+
+        hasBrackets = true
+    }
+
+    // Backspace
+    if(e.code === "Backspace" && hasBrackets) {
+        removeBrackets(node)
     }
 })
+
+function removeBrackets(node) {
+    var brackets = node.textContent
+
+    hasBrackets = false
+}
