5746b400ce64ba7fe805de631b41831ba9389185
L33dy <zbynekzidlicky@gmail.com>
2023-01-27 01:12:12 +0100

    auto-add second bracket

--- app/renderer/brackets.js
+++ app/renderer/brackets.js
@@ -1,39 +1,58 @@
 var editor = document.getElementById("editor")
 var hasBrackets = false
 
-editor.addEventListener('keydown', (e) => {
+editor.addEventListener('keyup', (e) => {
     var node = Editor.getCurrentNode()
     var currentBracket = ''
 
-    if(e.key === "(") {
-        node.innerText += ")"
-        currentBracket = "()"
-
-        hasBrackets = true
+    if (e.key === "(") {
+        addBracket(")")
     }
 
-    if(e.key === "{") {
-        node.innerText += "}"
-        currentBracket = "{}"
-
-        hasBrackets = true
+    if (e.key === "{") {
+        addBracket("}")
     }
 
-    if(e.key === "[") {
-        node.innerText += "]"
-        currentBracket = "[]"
-
-        hasBrackets = true
+    if (e.key === "[") {
+        addBracket("]")
     }
 
     // Backspace
-    if(e.code === "Backspace" && hasBrackets) {
-        removeBrackets(node)
+    if (e.code === "Backspace") {
+        removeBrackets()
+
+        console.log("removing brackets")
     }
 })
 
 function removeBrackets(node) {
-    var brackets = node.textContent
+    var sel = window.getSelection()
+    var range = sel.getRangeAt(0)
+    var text = range.startContainer.textContent
 
-    hasBrackets = false
+    var start = range.startOffset
+    var end = range.endOffset
+
+    if (text[start - 1] === "(" && text[end] === ")") {
+        range.startContainer.textContent = text.slice(0, start - 1) + text.slice(end + 1)
+
+        range.setStart(range.startContainer, start - 1)
+        range.setEnd(range.startContainer, start - 1)
+
+        sel.removeAllRanges()
+        sel.addRange(range)
+    }
+}
+
+function addBracket(bracket) {
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const newNode = document.createTextNode(bracket);
+
+    range.insertNode(newNode);
+    range.setStartBefore(newNode);
+    range.setEndBefore(newNode);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
 }
